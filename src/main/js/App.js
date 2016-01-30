06c28bbb7f1260d0bb837dddac77ae0c8aef3622
ohtomi <ohtomi.kenichi@gmail.com>
2016-01-31 01:01:15 +0900

    Refactoring

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -8,13 +8,58 @@ var ScatterPlot = require('./ScatterPlot');
 var BarChart = require('./BarChart');
 var TreeMap = require('./TreeMap');
 
+function generatePairData() {
+    var dataset = [];
+    var numDataPoints = 50;
+    var number1Range = Math.random() * 1000;
+    var number2Range = Math.random() * 1000;
+    for (var i = 0; i < numDataPoints; i++) {
+        var newNumber1 = Math.round(Math.random() * number1Range);
+        var newNumber2 = Math.round(Math.random() * number2Range);
+        dataset.push([newNumber1, newNumber2]);
+    }
+    return dataset;
+}
+
+function scatterPlotData() {
+    return generatePairData();
+}
+
+function barChartData() {
+    return generatePairData();
+}
+
+function treeMapData() {
+    var dows = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    var dataset = [];
+    for (var i = 0; i < 50; i++) {
+        var dow = Math.round(Math.random() * 7);
+        var hour = Math.round(Math.random() * 12);
+        dataset.push({
+            dayOfTheWeek: dows[dow],
+            hour: hour
+        });
+    }
+    return dataset;
+}
+
+function generateData(chartComponent) {
+    if (chartComponent === ScatterPlot) {
+        return scatterPlotData();
+    } else if (chartComponent === BarChart) {
+        return barChartData();
+    } else if (chartComponent === TreeMap) {
+        return treeMapData();
+    } else {
+        return [];
+    }
+}
+
 var App = React.createClass({
 
     getInitialState: function() {
         return {
-            chartType: 'scatterPlot',
             chartComponent: ScatterPlot,
-            refreshFunction: this.scatterPlotData,
             dataset: [],
             width: 800,
             height: 400,
@@ -24,73 +69,25 @@ var App = React.createClass({
     },
 
     componentDidMount: function() {
-        this.refreshData();
-    },
-
-    switchChartType: function(chartType) {
-        if (chartType === this.state.chartType) {
-            return;
-        }
-
-        var chartComponent, refreshFunction, dataset;
-
-        if (chartType === 'scatterPlot') {
-            chartComponent = ScatterPlot;
-        } else if (chartType === 'barChart') {
-            chartComponent = BarChart;
-        } else if (chartType === 'treeMap') {
-            chartComponent = TreeMap;
-        }
-
-        refreshFunction = this[chartType + 'Data'];
-        dataset = refreshFunction();
-
-        this.setState({
-            chartType: chartType,
-            chartComponent: chartComponent,
-            refreshFunction: refreshFunction,
-            dataset: dataset
-        });
-    },
-
-    refreshData: function() {
-        var dataset = this.state.refreshFunction();
+        var dataset = generateData(this.state.chartComponent);
         this.setState({dataset: dataset});
     },
 
-    _refreshPairData: function() {
-        var dataset = [];
-        var numDataPoints = 50;
-        var number1Range = Math.random() * 1000;
-        var number2Range = Math.random() * 1000;
-        for (var i = 0; i < numDataPoints; i++) {
-            var newNumber1 = Math.round(Math.random() * number1Range);
-            var newNumber2 = Math.round(Math.random() * number2Range);
-            dataset.push([newNumber1, newNumber2]);
+    refreshData: function() {
+        var dataset = generateData(this.state.chartComponent);
+        this.setState({dataset: dataset});
+    },
+
+    switchChartType: function(chartComponent) {
+        if (chartComponent === this.state.chartComponent) {
+            return;
         }
-        return dataset;
-    },
 
-    scatterPlotData: function() {
-        return this._refreshPairData();
-    },
-
-    barChartData: function() {
-        return this._refreshPairData();
-    },
-
-    treeMapData: function() {
-        var dows = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-        var dataset = [];
-        for (var i = 0; i < 100; i++) {
-            var dow = Math.round(Math.random() * 7);
-            var hour = Math.round(Math.random() * 12);
-            dataset.push({
-                dayOfTheWeek: dows[dow],
-                hour: hour
-            });
-        }
-        return dataset;
+        var dataset = generateData(chartComponent);
+        this.setState({
+            chartComponent: chartComponent,
+            dataset: dataset
+        });
     },
 
     render: function() {
@@ -98,34 +95,20 @@ var App = React.createClass({
             <div>
                 <div style={{width: '800px'}}>
                     <div style={{float: 'left'}}>
-                        {this.renderSwitchLinks()}
+                        <a href="#" onClick={this.switchChartType.bind(null, ScatterPlot)}>Scatter Plot</a>
+                        {' '}
+                        <a href="#" onClick={this.switchChartType.bind(null, BarChart)}>Bar Chart</a>
+                        {' '}
+                        <a href="#" onClick={this.switchChartType.bind(null, TreeMap)}>Tree Map</a>
                     </div>
                     <div style={{float: 'right'}}>
                         <a href="#" onClick={this.refreshData}>Refresh Data</a>
                     </div>
                 </div>
                 <div>
-                    {this.renderSpecifiedChart()}
+                    <this.state.chartComponent {...this.state} />
                 </div>
             </div>
-        );
-    },
-
-    renderSwitchLinks: function() {
-        return (
-            <span>
-                <a href="#" onClick={this.switchChartType.bind(null, 'scatterPlot')}>Scatter Plot</a>
-                {' '}
-                <a href="#" onClick={this.switchChartType.bind(null, 'barChart')}>Bar Chart</a>
-                {' '}
-                <a href="#" onClick={this.switchChartType.bind(null, 'treeMap')}>Tree Map</a>
-            </span>
-        );
-    },
-
-    renderSpecifiedChart: function() {
-        return (
-            <this.state.chartComponent {...this.state} />
         );
     }
 
